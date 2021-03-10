17fcfa8d53d4861e30dada938bf03b4de9753909
abdulwahidkhoeruddin <abdulwahidkhoeruddin@gmail.com>
2021-03-10 21:22:00 +0700

    perubahan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Identifikasi express dari dependencies pada package.json
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // Menjalankan express
 const app = express();
